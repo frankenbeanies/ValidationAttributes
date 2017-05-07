7286e5966c28e24e2c23a2a8c8b473dc1b4a8092
Samuel Bostick <me@samuelbostick.com>
2017-05-07 10:48:24 -0400

    feat(NotEmptyAttribute) - Added Not Empty Attribute
    - Added NotEmptyAttribute, which determines whether or not a collection is empty. If the collection is empty, it returns true. Otherwise, it returns false.

---
+++ Frankenbeanies.ValidationAttributes.Tests/NotEmptyAttributeTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Frankenbeanies.ValidationAttributes.Tests
+{
+    public class NotEmptyAttributeTests
+    {
+        NotEmptyAttribute GetSut()
+        {
+            return new NotEmptyAttribute();
+        }
+
+        [Fact]
+        public void NotEmptyAttribute_IsValid_NotICollection_Throws()
+        {
+            Assert.Throws<InvalidOperationException>(() => GetSut().IsValid(new { }));
+        }
+
+        [Fact]
+        public void NotEmptyAttribute_IsValid_Empty_ReturnsFalse()
+        {
+            Assert.False(GetSut().IsValid(new List<object> { }));
+        }
+
+        [Fact]
+        public void NotEmptyAttribute_IsValid_NotEmpty_ReturnsTrue()
+        {
+            Assert.True(GetSut().IsValid(new List<object> { new { } }));
+        }
+    }
+}
---
+++ Frankenbeanies.ValidationAttributes/NotEmptyAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frankenbeanies.ValidationAttributes
+{
+    /// <summary>
+    /// Determines whether or not a list is empty
+    /// </summary>
+    public class NotEmptyAttribute : ValidationAttribute
+    {
+#if SILVERLIGHT
+        internal
+#else
+        public
+#endif
+        override bool IsValid(object values)
+        {
+            if (!(values is ICollection)) throw new InvalidOperationException("NotEmptyAttribute requires an ICollection");
+
+            if ((values as ICollection).Count == 0) return false;
+
+            return true;
+        }
+    }
+}
